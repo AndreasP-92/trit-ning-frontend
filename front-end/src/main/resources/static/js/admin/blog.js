965dd2a88e6f7620e736f0e5b6c1039e42189c0d
SoSoCho <70580037+SoSoCho@users.noreply.github.com>
2021-05-21 19:31:42 +0200

    blog v.1

---
+++ front-end/src/main/resources/static/js/admin/blog.js
@@ -0,0 +1,67 @@
+const thisform = document.getElementById('');
+const author = document.getElementById('author');
+const dateTime = document.getElementById('dateTime');
+const description = document.getElementById('description');
+const image = document.getElementById('image');
+const title = document.getElementById('title');
+
+
+
+// ============== GET ACTIVITY ==============
+
+const mail = "kim@i-tritraening.dk";
+const myUrl = `http://localhost:5002/select/blogs`;
+
+const requestOptions = {
+    'content-type': 'application/json',
+    method: 'GET',
+    redirect: 'follow'
+};
+
+fetch(myUrl, requestOptions)
+    .then(response => response.json())
+    .then(data => {
+        gotBlogData(data)
+    })
+
+function gotBlogData(data){
+    data.forEach(fillTbody)
+}
+
+// ============== INSERT ACTIVITY ==============
+
+async function insertActivity(optionValues){
+    const filename = img.files[0].name;
+
+
+
+    fetch('http://localhost:5002/insert/blog', {
+        method: 'POST',
+        body: JSON.stringify({
+            'author'          : author.value,
+            'dateTime'        : dateTime.value,
+            'description'          : description.value,
+            'image'           : '/images/events/'+filename,
+            'title'           : title.value,
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        }
+    }).then(function (response) {
+        if (response.ok) {
+            return response.json();
+            console.log(response)
+        }
+        return Promise.reject(response);
+    }).then(function (data) {
+        console.log("AFTER INSERT=========",data.name)
+        console.log(data)
+        for(let i = 0; optionValues.length > i; i++){
+            console.log("OPTIONS====",optionValues[i])
+            insertDuration(optionValues[i], data.name)
+        }
+        thisForm.submit();
+    }).catch(function (error) {
+        console.warn('Something went wrong.', error);
+    });
+}
