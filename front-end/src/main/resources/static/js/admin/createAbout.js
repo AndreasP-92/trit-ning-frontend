179dad557c480258e41acf71c60afb5af00fc872
Rewax <daniel.lorenzen@hotmail.com>
2021-05-28 14:50:25 +0200

    Create Blog Blob

--- front-end/src/main/resources/static/js/admin/createAbout.js
+++ front-end/src/main/resources/static/js/admin/createAbout.js
@@ -1,23 +1,54 @@
-const img           = document.getElementById("aboutImg");
+const aboutImg      = document.getElementById("aboutImg");
 const description   = document.getElementById("editorCopy");
 const title         = document.getElementById("title");
 const thisForm      = document.getElementById("thisForm");
-
+const formData      = new FormData();
 
 thisForm.addEventListener('submit',async function (e){
     e.preventDefault();
 
-    insertAbout();
+    await insertAbout();
     console.log(description.value)
+})
+
+// ============ INSERT IMAGE FUNC
+async function insertImage(data){
+    formData.append("imageFile", aboutImg.files[0]);
+    formData.append("author_id", "0")
+    formData.append("page_id", "0")
+    formData.append("blog_id", "0")
+    formData.append("about_id", data.id)
+
+    console.log(img.files[0]);
+
+    const URL = "http://localhost:5002/image/upload"
+
+    const requestOptions = {
+        'content-type': 'multipart/form-data',
+        method: 'POST',
+        redirect: 'follow',
+        body: formData
+    };
+
+    fetch(URL,requestOptions)
+        .then(function (response){
+            console.log(response);
+        })
+        .catch(function (response){
+            console.log(response);
+        });
+}
+
+// ============ INSERT ABOUT FUNC
 
     async function insertAbout(){
         fetch('http://localhost:5002/insert/about', {
             method: 'POST',
             body: JSON.stringify({
 
-                'title'     : title.value,
+                'title'      : title.value,
                 'description': description.value,
-                'aboutImg'  : img.files[0].name,
+                'aboutImg'   : img.files[0].name,
             }),
             headers: {
                 'Content-type': 'application/json'
@@ -29,6 +60,7 @@ thisForm.addEventListener('submit',async function (e){
             }
             return Promise.reject(response);
         }).then(function (data) {
+            insertImage(data)
             console.log("AFTER INSERT=========", data.title)
             console.log(data)
 
@@ -37,5 +69,5 @@ thisForm.addEventListener('submit',async function (e){
             console.warn('Something went wrong.', error);
         });
     }
-})
+
 
