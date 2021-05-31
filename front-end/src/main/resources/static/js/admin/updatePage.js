4b7d83d192b104944defd9661a3909c8207893dc
Andreas Pedersen <31003640+AndreasP-92@users.noreply.github.com>
2021-05-31 02:46:12 +0200

    front update

--- front-end/src/main/resources/static/js/admin/updatePage.js
+++ front-end/src/main/resources/static/js/admin/updatePage.js
@@ -1,36 +1,52 @@
-const thisForm = document.getElementById('thisForm');
-const title = document.getElementById('title');
-const banner = document.getElementById('banner');
-const img = document.getElementById('img');
-const editor = document.getElementById('editor');
-const editorCopy = document.getElementById('editorCopy');
+const thisForm      = document.getElementById('thisForm');
+const title         = document.getElementById('title');
+const banner        = document.getElementById('banner');
+const bannerImgName = document.getElementById('bannerImgName')
+const img           = document.getElementById('img');
+const imgName       = document.getElementById('imgName');
+const editor        = document.getElementById('editor');
+const editorCopy    = document.getElementById('editorCopy');
+const warning       = document.getElementById('warning')
 
+const formData      = new FormData();
+const formData2     = new FormData();
 
-thePath = window.location.pathname;
-const urlTitle = thePath.substring(thePath.lastIndexOf('/')+1)
+// ======== INITIALIZE PAGE DATA ===========
+initPageData().then(data =>{
 
-const GetPageUrl = `http://localhost:5002/select/page/${urlTitle}`;
-
-console.log(urlTitle)
-
-const requestOptions = {
-    'content-type': 'application/json',
-    method: 'GET',
-    redirect: 'follow'
-}
-
-fetch(GetPageUrl, requestOptions)
-    .then(response => response.json())
-    .then(data => {
-        insertPageData(data);
-        console.log(data);
-    }).catch(function (e){
-        console.log(e);
+    insertPageData(data);
 })
 
+// ======== GET PAGE DATA ===========
+function initPageData(){
+    let thePath = window.location.pathname;
+    const urlTitle = thePath.substring(thePath.lastIndexOf('/')+1)
+
+    const GetPageUrl = `http://localhost:5002/select/page/${urlTitle}`;
+
+    console.log(urlTitle)
+
+    const requestOptions = {
+        'content-type': 'application/json',
+        method: 'GET',
+        redirect: 'follow'
+    }
+
+    return fetch(GetPageUrl, requestOptions)
+        .then(response => response.json())
+        .then((responseData) => {
+            // console.log(responseData);
+            return responseData;
+        })
+        .catch(error => console.warn(error));
+}
+
+// ========= INSERT PAGE DATA
 function insertPageData(data){
     title.value = data.title;
     editor.innerHTML = data.description;
+    bannerImgName.value = data.banner;
+    imgName.value = data.img;
     console.log(data.description)
 }
 
@@ -38,16 +54,35 @@ function insertPageData(data){
 thisForm.addEventListener('submit',async function (e) {
     e.preventDefault();
 
-    await updatePage();
+    if(banner.files[0] === undefined || img.files[0] === undefined || title.value === ""){
+        warning.textContent = "Udfyld alle felter"
+    }if(banner.files[0].name == img.files[0].name){
+        warning.textContent = "Billederne må ikke være ens"
+    }
+    else{
+        warning.setAttribute("style", "display:none;")
+        await updateActivity();
+    }
 
 })
 
-async function updatePage() {
+// ======== UPDATE ACTIVITY DATA ===========
+async function updateActivity() {
+    let checkBannerImg  = true;
+    let checkImg        = true;
 
+
+    if(banner.files[0] === undefined){
+        checkBannerImg = false;
+    }
+    if(img.files[0] === undefined){
+        checkImg = false;
+    }
+
+// CHECK EDITOR VALUE ===========
     let init;
-
     if (editorCopy.value == ""){
-         init = {
+        init = {
             method: 'PUT',
             body: JSON.stringify({
 
@@ -63,7 +98,7 @@ async function updatePage() {
 
 
     }else if (editorCopy.value != ""){
-         init = {
+        init = {
             method: 'PUT',
             body: JSON.stringify({
 
@@ -80,6 +115,7 @@ async function updatePage() {
         console.log(init)
     }
 
+// INITIALIZE UPDATE DATA ===========
     fetch(`http://localhost:5002/edit/page`,init)
         .then(function (response) {
             if (response.ok) {
@@ -88,12 +124,130 @@ async function updatePage() {
             }
             return Promise.reject(response);
         }).then(function (data) {
-        console.log("AFTER INSERT=========", data.title)
-        console.log(data)
+        console.log("AFTER INSERT=========", data)
 
-        // window.location.href = "/admin/index"
+
+// UPDATE BANNER IMAGE ===
+        if(checkBannerImg == true){
+            getBannerImg().then(bannerResponse => {
+                console.log("GET BANNER IMG ======", bannerResponse)
+                updateBannerImg(bannerResponse)
+            })
+        }
+
+// UPDATE IMAGE ===
+        if(checkImg == true){
+            getImg().then(imageResponse => {
+                console.log("GET IMG ======", imageResponse)
+                updateImage(imageResponse)
+            })
+        }
+
+// SUCCESS ===========
+        window.location.href = "/admin/index"
     }).catch(function (error) {
         console.warn('Something went wrong.', error);
     });
-
 }
+
+// ======== FETCH BANNER IMG DATA ===========
+function getBannerImg(){
+    let fetchImage = initPageData().then(pageData =>{
+        let init = {
+            method: "GET",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }
+        }
+
+        return fetch(`http://localhost:5002/image/get/imageid/${bannerImgName.value}/${pageData.id}`,init)
+            .then((response) => response.json())
+            .then((responseData) => {
+                console.log(responseData);
+                return responseData;
+            })
+            .catch(error => console.warn(error));
+    })
+    return fetchImage;
+}
+
+// ======== FETCH IMG DATA ===========
+function getImg(){
+    console.log("=========== GET IMAGE FETCH RUN ===========")
+    let fetchImage = initPageData().then(pageData =>{
+        let init = {
+            method: "GET",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }
+        }
+
+        return fetch(`http://localhost:5002/image/get/imageid/${imgName.value}/${pageData.id}`,init)
+            .then((response) => response.json())
+            .then((responseData) => {
+                console.log("IMAGE FETCH=========",responseData);
+                return responseData;
+            })
+            .catch(error => console.warn(error));
+    })
+    return fetchImage;
+}
+
+// ======== UPDATE BANNER IMG ===========
+function updateBannerImg(bannerImgData){
+    console.log("UPDATE BANNER DATA ====",bannerImgData)
+    formData.append("imageFile", banner.files[0]);
+    formData.append("image_id", bannerImgData.id)
+    formData.append("page_id", bannerImgData.pages.id)
+    formData.append("author_id", "0")
+
+    const URL1 = "http://localhost:5002/image/update"
+
+    const requestOptions = {
+        'content-type': 'multipart/form-data',
+        method: 'POST',
+        redirect: 'follow',
+        body: formData
+
+    };
+
+    fetch(URL1, requestOptions)
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (response) {
+            console.log(response);
+        });
+}
+
+// ======== UPDATE IMG ===========
+function updateImage(imgData){
+    console.log("UPDATE IMAGE DATA ====",imgData)
+
+    console.log("===============RUNNING UPLOAD IMAGE ===============")
+    formData2.append("imageFile", img.files[0]);
+    formData2.append("image_id", imgData.id)
+    formData2.append("page_id", imgData.pages.id)
+    formData2.append("author_id", "0")
+
+    const URL2 = "http://localhost:5002/image/update"
+
+    const requestOptions2 = {
+        'content-type': 'multipart/form-data',
+        method: 'POST',
+        redirect: 'follow',
+        body: formData2
+
+    };
+
+    fetch(URL2, requestOptions2)
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (response) {
+            console.log(response);
+        });
+}
+
